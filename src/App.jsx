3df6adce363f31350679f5c731f9072a3b36e2d5
Danielcosta <danielcfsilva@gmail.com>
2024-01-08 09:11:19 -0300

    escolher o tamanho da senha

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,15 @@ import { useState } from "react"
 function App() {
   const [password, setPassword] = useState("")
   const [copyText, setCopyText] = useState("Copiar")
+  const [passwordSize, setPasswordSize] = useState(12)
 
   function generatePassword() {
-    const newPassword = Math.random().toString(36).slice(-10);
+    const newPassword = Math.random().toString(36).slice(-passwordSize);
     setPassword(newPassword)
     setCopyText("Copiar")
   }
 
-  function copyToClioboard(){
+  function copyToClioboard() {
     navigator.clipboard.writeText(password)
     setCopyText("Copiado!")
   }
@@ -18,9 +19,19 @@ function App() {
   return (
     <div className="app">
       <h1>Gerador de Senha</h1>
-      <button onClick={generatePassword}>Gerar!</button>
+      <div>
+        <label htmlFor="passwordSize">Tamanho: </label>
+        <input
+          type="number"
+          id="passwordSize"
+          min={1}
+          value={passwordSize}
+          onChange={(ev)=> setPasswordSize(ev.target.value) }
+        />
+      </div>
+      <button onClick={generatePassword}>Gerar senha de {passwordSize} caracteres</button>
       <button onClick={copyToClioboard}>{copyText}</button>
-      <div>{password}</div>
+      <div>Sua senha segura: {password}</div>
     </div>
 
   )
